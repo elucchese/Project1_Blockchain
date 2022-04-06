2e9878351ecec93833ee092ebdcf828f1aec706e
Emily Lucchese <emilylucchese@Emilys-MacBook-Pro.local>
2022-04-06 09:30:12 -0700

    Initial commit

---
+++ src/P2PMessageQueue.java
@@ -0,0 +1,51 @@
+/**
+ * This Queue maintains the queue of messages coming from connected clients.
+ */
+public class P2PMessageQueue {
+
+    private P2PMessage head = null;
+    private P2PMessage tail = null;
+
+
+    /**
+     * This method allows adding a message object to the existing queue.
+     * @param oMessage
+     */
+    public synchronized void enqueue(P2PMessage oMessage){
+
+        if (head == null) {
+            head = oMessage;
+            tail = oMessage;
+        } else {
+            tail.next = oMessage;
+            tail = oMessage;
+        }
+    }
+
+
+    /**
+     * This method allows removing a message object from the existing queue.
+     * @return
+     */
+    public synchronized P2PMessage dequeue(){
+
+		if (head == null){
+            return null;
+        } else {
+            P2PMessage oTemp = head;
+            head = head.next;
+            return oTemp;
+        }
+    }
+
+
+    public boolean hasNodes(){
+
+		if(head == null){
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
